973f29792c4c7ce9e5a15bd682a9fdcfcbcfae34
Natalia Garcia <nagarcia.docente@gmail.com>
2022-08-08 20:30:22 +0200

    add the token generated with the GeoCoding api and tested in postman

--- models/search.js
+++ models/search.js
@@ -14,7 +14,7 @@ const axios = require('axios')
             //petición http
             //console.log('Ciudad', lugar)
     
-            const resp = await axios.get('https://reqres.in/api/users?page=2')
+            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/rosar.json?access_token=pk.eyJ1IjoibmF0aGdhcmNpYTI5MDMiLCJhIjoiY2w2bDF6MWd0MDdqZTNibW91NG02d2w3ZyJ9.9G7pPFD3gmCffispUbmlIw&limit=5&types=place%2Cpostcode%2Caddress&language=es')
             console.log(resp.data) 
     
             return [] 
