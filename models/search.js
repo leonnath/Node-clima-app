d4044e9c49c5a97c02607af2161ac7dd6c7c6faf
Natalia Garcia <nagarcia.docente@gmail.com>
2022-08-09 11:04:30 +0200

    instance axios

--- models/search.js
+++ models/search.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
- class Busquedas {
+class Busquedas {
 
     historial = ['Tegucigalpa', 'Madrid', 'San José']
 
@@ -8,21 +8,33 @@ const axios = require('axios')
         //TODO leer DB si existe
     }
 
-    async ciudad( lugar = '' ) {
+    get paramsMapbox() {
+        return {
+            'access_token': 'pk.eyJ1IjoibmF0aGdhcmNpYTI5MDMiLCJhIjoiY2w2bDF6MWd0MDdqZTNibW91NG02d2w3ZyJ9.9G7pPFD3gmCffispUbmlIw',
+            'limit': 5,
+            'language': 'es'    
+        }
+    }
+
+    async ciudad(lugar = '') {
 
         try {
             //petición http
-            //console.log('Ciudad', lugar)
-    
-            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/rosar.json?access_token=pk.eyJ1IjoibmF0aGdhcmNpYTI5MDMiLCJhIjoiY2w2bDF6MWd0MDdqZTNibW91NG02d2w3ZyJ9.9G7pPFD3gmCffispUbmlIw&limit=5&types=place%2Cpostcode%2Caddress&language=es')
-            console.log(resp.data) 
-    
-            return [] 
-            
+
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox
+            })
+
+            const resp = await instance.get()
+            console.log(resp.data)
+
+            return []
+
         } catch (error) {
             return [] //Retornar todos los lugares que coincidan con el escrito por la persona
         }
-        return [] 
+        return []
     }
 
 }
